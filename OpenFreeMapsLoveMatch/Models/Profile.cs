f47922ff666645152cdf56130eeffc6acf80833b
Tim Dekker <211560602+Dek-T1995@users.noreply.github.com>
2026-04-21 22:52:32 +0200

    Added Bio back - Test

--- OpenFreeMapsLoveMatch/Models/Profile.cs
+++ OpenFreeMapsLoveMatch/Models/Profile.cs
@@ -6,6 +6,7 @@
         public string? Name { get; set; }
         public int Age { get; set; }
         public string? City { get; set; }
+        public string? Bio { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
     }
